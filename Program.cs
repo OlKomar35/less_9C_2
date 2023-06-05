cac6f825ad5b39acbedc65caabec0f922fec85f8
Olga Komar <komar_ol@mail.ru>
2023-06-05 17:21:14 +0300

    task 2

--- Program.cs
+++ Program.cs
@@ -3,3 +3,25 @@
 
 //M = 1; N = 15 -> 120
 //M = 4; N = 8. -> 30
+
+int SumNums(int m, int n)
+{
+    if (n == m) { return n; }
+    else if (n < m)
+    {
+        return n + SumNums(n + 1, m);
+    }
+    else
+    {
+        return m + SumNums(m + 1, n);
+    }
+}
+
+Console.Write("Введите m= ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите n= ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = SumNums(m, n);
+Console.WriteLine($"Сумму натуральных элементов в промежутке от M до N= {result}");
